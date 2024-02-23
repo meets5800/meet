d90cc90186c3b50852c00441425e9c8935da1a5a
meets5800 <meetp3775@gmail.com>
2024-02-23 16:19:11 +0530

    last commit

--- src/Components/Home/HomeButtons.jsx
+++ src/Components/Home/HomeButtons.jsx
@@ -4,6 +4,10 @@ import "../Css/homebuttons.css"
 export const HomeButtons = () => {
     const [isVisible, setIsVisible] = useState(false);
 
+    const handleCall = () => {
+      window.location.href = 'tel:8238055800';
+    };
+
 useEffect(() => {
   // Delaying the visibility of the first data
   setTimeout(() => {
@@ -15,7 +19,7 @@ useEffect(() => {
           <MDBBtn color="info sm:w-[150px] sm:text-[15px] w-[75px]text-[8px] ">
           <a href="mailto:meetp3775@gmail.com" className="text-white font-[700]">Hire me</a></MDBBtn>
           <MDBBtn color="info sm:w-[150px] sm:text-[15px] w-[75px]text-[8px]">
-          <a href= "https://meets5800.github.io/meet/contact" className="text-white font-[700]">{`Let's Talk`}</a>
+          <a onClick={handleCall}className="text-white font-[700]">{`Let's Talk`}</a>
           </MDBBtn>
     </div>
   )
